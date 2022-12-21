9f1b9b56097cb6bd0432003f2cb7d35b6c4aafe2
Faizan Malik <78931488+malikfaizan403@users.noreply.github.com>
2022-12-21 23:43:11 +0500

    HameedFoundation Desktop

---
+++ assets/js/custom.js
@@ -0,0 +1,30 @@
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+    var header = $('.navbar').height();
+    if (scroll >= header) {
+        $("header").addClass("sticky_header");
+    } else {
+        $("header").removeClass("sticky_header");
+    }
+});
+
+// Counter To Count Number Visit
+var height = 0;
+$(window).scroll(function() {
+
+  var oTop = $('#counter').offset().top - window.innerHeight;
+  // Md.Asaduzzaman Muhid
+  if (height == 0 && $(window).scrollTop() > oTop) {
+    $('.percent-value').each(function() {
+        var $this = $(this);
+        jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+            duration: 1500,
+            easing: 'swing',
+            step: function () {
+                $this.text(Math.ceil(this.Counter));
+            }
+        });
+    });
+    height = 1;// Md.Asaduzzaman Muhid
+  }
+});
